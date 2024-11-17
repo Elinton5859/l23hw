50607b6668da9c183cc7dda8ee4216e1207b7320
Ethan <elinton5859@floridapoly.edu>
2024-11-17 14:32:41 -0500

    Changed frequency of the sinwave function

--- sinwave_func.py
+++ sinwave_func.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #initializing the sinwave
-x = np.arange(0, 5*np.pi, 0.1)
+x = np.arange(0, 10*np.pi, 0.1)
 y = np.sin(x)
-#plotting ti via the plot function in matplotlib.pyplot library
+#plotting to via the plot function in matplotlib.pyplot library
 plt.plot(x, y, color='green')
 plt.show()
 
